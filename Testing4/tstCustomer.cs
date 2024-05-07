4e7a402fe855cffa293ca419e087a17a5e029a7d
p2731165 <mominmukhtarali1@gmail.com>
2024-05-07 09:47:58 +0100

    added testing for Customer class

---
+++ Testing4/tstCustomer.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //crat an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
+
+
+

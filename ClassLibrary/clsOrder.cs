5680cdeb2d7f53e486c37c22c22335518af13c99
Umesh . <P2735550@my365.dmu.ac.uk>
2024-05-07 10:55:13 +0100

    added testing in order class

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,10 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }

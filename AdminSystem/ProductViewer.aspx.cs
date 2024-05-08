5629ff53b967ff0ac5e3d16a4128c22ab937c958
Kittipat Weng <P2754832@my365.dmu.ac.uk>
2024-05-08 12:54:27 +0100

    Adding a Simple Presentation Layer in product session

--- AdminSystem/ProductViewer.aspx.cs
+++ AdminSystem/ProductViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsGame
+        clsGame AnGame = new clsGame();
+        //get the data from the session object
+        AnGame = (clsGame)Session["Angame"];
+        //display the game title for this entry
+        Response.Write(AnGame.GameTitle);
     }
 }

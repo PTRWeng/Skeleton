52f51cb95448559a65433b66cb1a663223f95de1
p2725585 <p2725585@GWHXX283WIN018W>
2024-05-08 13:03:21 +0100

    add testing to staff class

---
+++ Testing1/tstStaff.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsStaff AnStaff = new clsStaff();
+            Assert.IsNotNull(AnStaff);
+
+        }
+        [TestMethod]
+        public void ActivePropertyOk()
+        {
+            clsStaff AnStaff2 = new clsStaff();
+            Boolean TestData = true;
+            AnStaff2.Active = TestData;
+            Assert.AreEqual(AnStaff2.Active, TestData);
+
+
+
+        }
+    }
+}

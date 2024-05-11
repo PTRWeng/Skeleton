33b920c9ad92f34e812aed783b5608b7bcb31631
p2725585 <p2725585@GWHXX283WIN018W>
2024-05-12 00:19:19 +0100

    Testing table

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,7 +1,16 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
         public bool Active { get; set; }
+        public int StaffID { get; set; }
+        public string StaffFirstName { get; set; }
+        public string StaffLastName { get; set; }
+        public DateTime StaffDateofBirth { get; set; }
+        public string StaffNumber { get; set; }
+        public int StaffAddress { get; set; }
+        public string StaffEmail { get; set; }
     }
 }

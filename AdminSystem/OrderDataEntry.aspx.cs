3ea5ef34ae28daeafe680ee3cc9a3517c1d871e0
Umesh . <P2735550@my365.dmu.ac.uk>
2024-05-30 11:27:45 +0100

    erqgt

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -16,12 +16,6 @@ public partial class _1_DataEntry : System.Web.UI.Page
     }
 
 
-
-    protected void btnFind_Click(object sender, EventArgs e)
-    {
-        // Create an instance of the order class
-        clsOrder AnOrder = new clsOrder();
-        // Create a variable to store the primary key
         Int32 orderId;
         // Create a variable to store the result of the find operation
         Boolean Found = false;

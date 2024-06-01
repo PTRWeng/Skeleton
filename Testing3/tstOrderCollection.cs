d9556fef2b7d4ba532c82cbb7f735cca6b1cabb6
umesh batalvi <104628837+celestialcracker@users.noreply.github.com>
2024-06-01 20:03:46 +0100

    Update tstOrderCollection.cs

--- Testing3/tstOrderCollection.cs
+++ Testing3/tstOrderCollection.cs
@@ -85,15 +85,173 @@ namespace Testing3
             //add the time to the test list
             TestList.Add(TestItem);
             //assign the data to the property
-            AllOrder.OrderList = TestList;
+            AllOrder.orderList = TestList;
             //test to see that the two values are the same
-            Assert.AreNotEqual(AllOrder.Count, TestList.Count);
+            Assert.AreEqual(AllOrder.Count, TestList.Count);
         }
 
-        
 
-        
+        [TestMethod]
+        public void AddMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrder = new clsOrderCollection();
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //variable to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.orderStatus = true;
+            TestItem.orderId = 1;
+            TestItem.orderDescription = "The Last Of Us";
+            TestItem.orderQuantity = 2;
+            TestItem.orderDate = DateTime.Now;
+            TestItem.orderAmount = 13;
+            TestItem.shippingAddress = "25 grasmere street";
+            //set Thisorder to the test data
+            AllOrder.ThisOrder = TestItem;
+            //add the record
+            PrimaryKey = AllOrder.Add();
+            //set the primary key of the test data
+            TestItem.orderId = PrimaryKey;
+            //find the record
+            AllOrder.ThisOrder.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrder.ThisOrder, TestItem);
+        }
 
+
+
+        [TestMethod]
+        public void UpdateMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrder = new clsOrderCollection();
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //variable to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.orderStatus = true;
+            TestItem.orderId = 1;
+            TestItem.orderDescription = "The Last Of Us";
+            TestItem.orderQuantity = 2;
+            TestItem.orderDate = DateTime.Now;
+            TestItem.orderAmount = 13;
+            TestItem.shippingAddress = "25 grasmere street";
+            //set Thisorder to the test data
+            AllOrder.ThisOrder = TestItem;
+            //add the record
+            PrimaryKey = AllOrder.Add();
+            //set the primary key of the test data
+            TestItem.orderId = PrimaryKey;
+            //modify the test record
+            TestItem.orderStatus = true;
+            TestItem.orderId = 1;
+            TestItem.orderDescription = "The Last Of Us";
+            TestItem.orderQuantity = 1;
+            TestItem.orderDate = DateTime.Now;
+            TestItem.orderAmount = 15;
+            TestItem.shippingAddress = "25 grasmere street";
+            //set the record based on the new test data
+            AllOrder.ThisOrder = TestItem;
+            //update the record
+            AllOrder.Update();
+            //find the record
+            AllOrder.ThisOrder.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrder.ThisOrder, TestItem);
+        }
+
+
+        [TestMethod]
+        public void DeleteMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrder = new clsOrderCollection();
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //variable to store the primary key
+            Int32 PrimaryKey = 0;
+            //set its properties
+            TestItem.orderStatus = true;
+            TestItem.orderId = 1;
+            TestItem.orderDescription = "The Last Of Us";
+            TestItem.orderQuantity = 2;
+            TestItem.orderDate = DateTime.Now;
+            TestItem.orderAmount = 13;
+            TestItem.shippingAddress = "25 grasmere street";
+            //set Thisorder to the test data
+            AllOrder.ThisOrder = TestItem;
+            //add the record
+            PrimaryKey = AllOrder.Add();
+            //set the primary key of the test data
+            TestItem.orderId = PrimaryKey;
+            //find the record
+            AllOrder.ThisOrder.Find(PrimaryKey);
+            //delete the record
+            AllOrder.Delete();
+            //now find the record
+            Boolean Found = AllOrder.ThisOrder.Find(PrimaryKey);
+            //test to see that the two values are the same
+            Assert.IsFalse(Found);
+        }
+
+
+        [TestMethod]
+        public void ReportByorderDescriptionMethodOK()
+        {
+            //create an instance of the class containing unfiltered results
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create an instance of the filtered data
+            clsOrderCollection FilteredOrders = new clsOrderCollection();
+            //apply a blank string (should return all records);
+            FilteredOrders.ReportByorderDescription("");
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.Count, FilteredOrders.Count);
+        }
+
+        [TestMethod]
+        public void ReportByorderDescriptionNoneFound()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection FilteredOrders = new clsOrderCollection();
+            //apply a order title that doesn't exist
+            FilteredOrders.ReportByorderDescription("yyyyyyy");
+            //test to see that there are no records
+            Assert.AreEqual(0, FilteredOrders.Count);
+        }
+
+        [TestMethod]
+        public void ReportByorderDescriptionTestDataFound()
+        {
+            //create an instance of the filtered data
+            clsOrderCollection FilteredOrders = new clsOrderCollection();
+            //apply a order title that doesn't exist
+            FilteredOrders.ReportByorderDescription("xxxxxx");
+            //variable to store the outcome
+            Boolean OK = true;
+            //check that the correct number of records are found
+            if (FilteredOrders.Count == 2)
+            {
+                //check to see that the first record in 26
+                if (FilteredOrders.orderList[0].orderId != 26)
+                {
+                    OK = false;
+                }
+                //check to see that the first record is 28
+                if (FilteredOrders.orderList[1].orderId != 28)
+                {
+                    OK = false;
+                }
+            }
+            else
+            {
+                OK = false;
+            }
+            //test to see that there are no record
+            Assert.IsFalse(OK);
+        }
     }
 }
 

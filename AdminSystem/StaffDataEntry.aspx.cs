1e1a37690b4ac9f74bbc8e5e2716c4c78b4fa774
Hamza Yaqoob <p2725585@my365.dmu.ac.uk>
2024-06-04 20:19:43 +0100

    Staff Prac11

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -14,8 +15,50 @@ public partial class _1_DataEntry : System.Web.UI.Page
     }
 
     protected void btnOK_Click(object sender, EventArgs e)
-    { 
+    {
+        clsStaff AStaff = new clsStaff();
 
+
+        string StaffFirstName = txtStaffFirstName.Text;
+        string StaffLastName = txtStaffLastName.Text;
+        string StaffDateofBirth = txtStaffDateofBirth.Text;
+        string StaffNumber = txtStaffNumber.Text;
+        string StaffAddress = txtStaffAddress.Text;
+        string StaffEmail = txtStaffEmail.Text;
+        string Error = "";
+        Error = AStaff.Valid(StaffFirstName, StaffLastName, StaffDateofBirth, StaffNumber, StaffAddress, StaffEmail);
+        if (Error == "")
+        {
+            AStaff.StaffFirstName = StaffFirstName;
+            AStaff.StaffLastName = StaffLastName;
+            AStaff.StaffDateofBirth = Convert.ToDateTime(StaffDateofBirth);
+            AStaff.StaffNumber = StaffNumber;
+            AStaff.StaffAddress = StaffAddress;
+            AStaff.StaffEmail = StaffEmail;
+            Session["AStaff"] = AStaff;
+
+
+            /*
+             clsStaffCollection StaffList = new clsStaffCollection();
+            if (StaffID == -1)
+            {
+                StaffList.ThisStaff = AStaff;
+                StaffList.Add();
+            }
+            else
+            {
+                StaffList.ThisStaff.Find(StaffID);
+                StaffList.ThisStaff = AStaff;
+                StaffList.Update();
+            }
+            Response.Redirect("StaffsList.aspx"); 
+              
+             */
+        }
+        else
+        {
+            lblError.Text = Error;
+        }
     }
 
     protected void btnFind_Click(object sender, EventArgs e)
@@ -35,5 +78,10 @@ public partial class _1_DataEntry : System.Web.UI.Page
             txtStaffEmail.Text = AStaff.StaffEmail.ToString();
 
         }
+    }
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("StaffList.aspx");
     }
 }

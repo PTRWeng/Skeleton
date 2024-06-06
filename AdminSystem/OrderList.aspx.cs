769429747af7d3b63845b3dd16a974fe3dba25d5
umesh batalvi <104628837+celestialcracker@users.noreply.github.com>
2024-06-06 12:56:51 +0100

    Update OrderList.aspx.cs

--- AdminSystem/OrderList.aspx.cs
+++ AdminSystem/OrderList.aspx.cs
@@ -30,9 +30,9 @@ public partial class _1_List : System.Web.UI.Page
 
     void DisplayOrders()
     {
-        //create an instance of the Game collection
+        //create an instance of the order collection
         clsOrderCollection Orders = new clsOrderCollection();
-        //set the data source to list of games in the cllection
+        //set the data source to list of order in the cllection
         lstorderList.DataSource = Orders.orderList;
         //set the name of the primary key
         lstorderList.DataValueField = "orderId";
@@ -42,32 +42,44 @@ public partial class _1_List : System.Web.UI.Page
         lstorderList.DataBind();
     }
 
-    protected void btnAdd_Click(object sender, EventArgs e)
+    protected void btnReturnToMainMenu_Click(object sender, EventArgs e)
     {
-        //store -1 into the session object to indicate this ia a new record
-        Session["orderId"] = -1;
-        //redirect to the data entry page
-        Response.Redirect("OrderDataEntry.aspx");
+        //redirect the user to the main menu page
+        Response.Redirect("TeamMainMenu.aspx");
     }
 
-    protected void btnEdit_Click(object sender, EventArgs e)
+    protected void btnClearFilter_Click(object sender, EventArgs e)
     {
-        //variable to store the primary key value of the ecord to be edited
-        Int32 orderId;
-        //if a record has been selected from the list
-        if (lstorderList.SelectedIndex != -1)
-        {
-            //get the primary key value of the record to edit
-            orderId = Convert.ToInt32(lstorderList.SelectedValue);
-            //store the data in the session object
-            Session["orderId"] = orderId;
-            //redirect to the edit page
-            Response.Redirect("OrderDataEntry.aspx");
-        }
-        else   //if no record has been selected
-        {
-            lblError.Text = "Please select a record from the list to edit";
-        }
+        //create an instance of the order object
+        clsOrderCollection AnOrder = new clsOrderCollection();
+        //set an empty string
+        AnOrder.ReportByorderDescription("");
+        //clear any existing filter to tidy up the interface
+        txtFilter.Text = "";
+        //set the data source to the list of orders in the collection
+        lstorderList.DataSource = AnOrder.orderList;
+        //set the name of the primary key
+        lstorderList.DataValueField = "orderId";
+        //set the name of the feild to display
+        lstorderList.DataTextField = "orderDescription";
+        //bind the data to the list
+        lstorderList.DataBind();
+    }
+
+    protected void btnApplyFilter_Click(object sender, EventArgs e)
+    {
+        //create an instance of the order object
+        clsOrderCollection AnOrder = new clsOrderCollection();
+        //retrieve the value of order title from the presentation layer
+        AnOrder.ReportByorderDescription(txtFilter.Text);
+        //set the data source to the list of order in the collection
+        lstorderList.DataSource = AnOrder.orderList;
+        //set the name of the primary key
+        lstorderList.DataValueField = "orderId";
+        //set the name of the feild to display
+        lstorderList.DataTextField = "orderDescription";
+        //bind the data to the list
+        lstorderList.DataBind();
     }
 
     protected void btnDelete_Click(object sender, EventArgs e)
@@ -91,43 +103,31 @@ public partial class _1_List : System.Web.UI.Page
         }
     }
 
-    protected void btnApplyFilter_Click(object sender, EventArgs e)
+    protected void btnEdit_Click(object sender, EventArgs e)
     {
-        //create an instance of the game object
-        clsOrderCollection AnOrder = new clsOrderCollection();
-        //retrieve the value of game title from the presentation layer
-        AnOrder.ReportByorderDescription(txtFilter.Text);
-        //set the data source to the list of games in the collection
-        lstorderList.DataSource = AnOrder.orderList;
-        //set the name of the primary key
-        lstorderList.DataValueField = "orderId";
-        //set the name of the feild to display
-        lstorderList.DataTextField = "orderDescription";
-        //bind the data to the list
-        lstorderList.DataBind();
+        //variable to store the primary key value of the ecord to be edited
+        Int32 orderId;
+        //if a record has been selected from the list
+        if (lstorderList.SelectedIndex != -1)
+        {
+            //get the primary key value of the record to edit
+            orderId = Convert.ToInt32(lstorderList.SelectedValue);
+            //store the data in the session object
+            Session["orderId"] = orderId;
+            //redirect to the edit page
+            Response.Redirect("OrderDataEntry.aspx");
+        }
+        else   //if no record has been selected
+        {
+            lblError.Text = "Please select a record from the list to edit";
+        }
     }
 
-    protected void btnClearFilter_Click(object sender, EventArgs e)
+    protected void btnAdd_Click(object sender, EventArgs e)
     {
-        //create an instance of the order object
-        clsOrderCollection AnOrder = new clsOrderCollection();
-        //set an empty string
-        AnOrder.ReportByorderDescription("");
-        //clear any existing filter to tidy up the interface
-        txtFilter.Text = "";
-        //set the data source to the list of orders in the collection
-        lstorderList.DataSource = AnOrder.orderList;
-        //set the name of the primary key
-        lstorderList.DataValueField = "orderId";
-        //set the name of the feild to display
-        lstorderList.DataTextField = "orderDescription";
-        //bind the data to the list
-        lstorderList.DataBind();
-    }
-
-    protected void btnReturnToMainMenu_Click(object sender, EventArgs e)
-    {
-        //redirect the user to the main menu page
-        Response.Redirect("TeamMainMenu.aspx");
+        //store -1 into the session object to indicate this ia a new record
+        Session["orderId"] = -1;
+        //redirect to the data entry page
+        Response.Redirect("OrderDataEntry.aspx");
     }
 }

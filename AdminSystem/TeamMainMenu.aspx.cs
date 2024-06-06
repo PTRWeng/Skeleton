c55ef9a074daf99fb7a0871a7dd43050db5c9c74
LilMortal <104632298+LilMortal@users.noreply.github.com>
2024-06-06 12:48:15 +0100

    Update TeamMainMenu.aspx.cs

--- AdminSystem/TeamMainMenu.aspx.cs
+++ AdminSystem/TeamMainMenu.aspx.cs
@@ -19,8 +19,8 @@ public partial class TeamMainMenu : System.Web.UI.Page
     }
 
     protected void btnCustomers_Click(object sender, EventArgs e)
-{
+    {
     //redirect the user to the game product login page
     Response.Redirect("CustomerLogin.aspx");
-}
+    }
 }
